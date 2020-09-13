bf323ed22f4b9ec4798e6e1f65ca133e9392794f
Gaurav <gauravjangam444@gmail.com>
2020-09-13 21:05:43 +0530

    build folder added

---
+++ build/precache-manifest.590d9a8df312fb8571b3fd1487b29bef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c9207e00d6a8062db9dc8c9ac199c6b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c024a6cbfa7b66daf13",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "1524f5b4213666b05ff8",
+    "url": "/static/js/2.d3204a84.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.d3204a84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c024a6cbfa7b66daf13",
+    "url": "/static/js/main.444e8a26.chunk.js"
+  },
+  {
+    "revision": "27dc777568247efc1357",
+    "url": "/static/js/runtime-main.c5abf43b.js"
+  }
+]);
